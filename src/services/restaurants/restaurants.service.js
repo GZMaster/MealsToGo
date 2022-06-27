541660e06b8bd1f3b73063d88800d0ec6d9f6841
GZMaster <danigamester@gmail.com>
2022-06-27 19:08:49 +0100

    including camelize dependency

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
@@ -10,9 +11,15 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
+
 restaurantsRequest()
-  .then((result) => {
-    console.log(result);
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
   })
   .catch((err) => {
     console.log("error");
