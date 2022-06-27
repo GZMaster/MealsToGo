7178281abd08f2fb74177798ad64a76466486066
GZMaster <danigamester@gmail.com>
2022-06-27 18:34:39 +0100

    setting up services

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
+import { restaurantsRequest } from './src/services/restaurants/restaurants.service'
 
 const Tab = createBottomTabNavigator();
 
