608757cc55c7bb3be1fa5608b30596532a10b78b
GZMaster <danigamester@gmail.com>
2022-07-13 02:19:19 +0100

    installing firebase

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -12,7 +14,35 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { Navigation } from "./src/infrastructure/navigation/index";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA71rd6UgyKmldRzynNwZ1pyCfSgnY2ss0",
+  authDomain: "mealstogo-c23dc.firebaseapp.com",
+  projectId: "mealstogo-c23dc",
+  storageBucket: "mealstogo-c23dc.appspot.com",
+  messagingSenderId: "607882177935",
+  appId: "1:607882177935:web:c22b9f81aeb257ebbbaffa",
+};
+
+if (!getApps.length) {
+  initializeApp(firebaseConfig);
+}
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const auth = getAuth();
+
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, "admin@test.com", "testadmin")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }, 2000);
+  }, []);
+
   let [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
