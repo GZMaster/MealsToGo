6a20e884e3edbd8c09b1cd6249909f34b6807bd8
GZMaster <danigamester@gmail.com>
2022-07-15 15:24:10 +0100

    main page styling

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,6 +1,34 @@
 import React from "react";
-import { AccountBackground } from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+} from "../components/account.styles";
 
-export const AccountScreen = () => {
-  return <AccountBackground />;
+export const AccountScreen = ({ navigation }) => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon="account-circle-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="account-circle-outline"
+            mode="contained"
+            onPress={() => navigation.navigate("Register")}
+          >
+            Sign Up
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -8,8 +8,8 @@ const Stack = createStackNavigator();
 
 export const AccountNavigator = () => (
   <Stack.Navigator headerMode="none">
-    <Stack.Screen name="Main" component={() => <AccountScreen />} />
-    <Stack.Screen name="Login" component={() => <LoginScreen />} />
-    <Stack.Screen name="Register" component={() => <RegisterScreen />} />
+    <Stack.Screen name="Main" component={AccountScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
 );
